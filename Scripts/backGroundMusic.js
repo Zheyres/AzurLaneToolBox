3a195a003e614f1d8d3046d0bd34f4cf2b1cb015
Gonzalo Francisco Hitta <darknavy_noth1ng@hotmail.com>
2024-04-15 20:53:00 -0300

    first edition of azurlane AzurLaneToolBoxWeb

---
+++ Scripts/backGroundMusic.js
@@ -0,0 +1,32 @@
+// backGroundMusic.js
+
+// Initialize the audio element
+var audio = new Audio('Assets/backgroundMusicMainPage.weba');
+audio.loop = true;
+audio.volume = 0.3;
+// Event listener for the play button
+$(document).ready(function() {
+    $('.play-btn').click(function() {
+        // Checks if the audio is currently paused
+        // If it is, plays the audio and changes the button text to "Pause"
+        // If it isn't, pauses the audio and changes the button text to "Play"
+        if (audio.paused) {
+            audio.play();
+            $('.play-btn').html('Pause');
+        } else {
+            audio.pause();
+            $('.play-btn').html('Play');
+        }
+    });
+});
+
+// Event listener for the volume slider
+$('.volume-slider').on('input', function() {
+    // Update the volume of the audio element
+    audio.volume = $(this).val() / 100;
+});
+
+// Play the audio when the page loads
+$(document).ready(function() {
+    audio.play();
+});
